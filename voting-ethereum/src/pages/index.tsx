4ec51a8f277fbd6d029391817250f960e29c31bc
luucas-melo <lucasmelodos322@gmail.com>
2023-05-02 14:32:26 -0300

    Add web3
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- voting-ethereum/src/pages/index.tsx
+++ voting-ethereum/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Wallet from "@/components/Wallet";
 import { useListen } from "@/hooks/useListen";
+import { abi } from "@/constants/abi";
 import { useMetamask } from "@/hooks/useMetamask";
 import { Button } from "@chakra-ui/react";
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +16,7 @@ export default function Home() {
   } = useMetamask();
 
   const listen = useListen();
-
+  const web3 = new Web3("ws://localhost:8545");
   useEffect(() => {
     if (typeof window !== undefined) {
       // start by checking if window.ethereum is present, indicating a wallet extension
@@ -40,8 +42,88 @@ export default function Home() {
       dispatch({ type: "pageLoaded", isMetamaskInstalled, wallet, balance });
     }
   }, []);
+
+  const [contract, setContract] = useState(null);
+
+  let contractAddress = "0x1f49444De78Bd325C2A3C1C4550fF6BBF5FbEEd1"; // hard coded for now
+
+  const add = async () => {
+    if (!wallet) return;
+
+    const contract = new web3.eth.Contract(abi, contractAddress);
+
+    const inputConsts = "0xDf3f47571FD69590dA954777562E8751869c9817"; // hard coded for now
+
+    const data = contract.methods.addToWhiteList(inputConsts).encodeABI();
+    console.log("data", data);
+    // Define the gas price and gas limit
+    const gasPrice = await web3.eth.getGasPrice();
+    console.log("gasPrice", gasPrice);
+    console.log("wallet", wallet);
+    const tx = {
+      from: wallet,
+      to: contractAddress,
+      data,
+      gas: "2000000000",
+      gasPrice,
+    };
+
+    let gasLimit = await web3.eth.estimateGas(tx);
+    console.log("gasLimit", gasLimit);
+    tx.gas = (gasLimit * 1.5).toFixed(0);
+
+    const res = await web3.eth.sendTransaction(tx, (error, transactionHash) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(`Transaction hash: ${transactionHash}`);
+      }
+    });
+  };
+
+  const vote = async () => {
+    if (!wallet) return;
+
+    const contract = new web3.eth.Contract(abi, contractAddress);
+
+    const inputConsts = 1;
+
+    const data = contract.methods.vote(inputConsts).encodeABI();
+    console.log("data", data);
+    // Define the gas price and gas limit
+    const gasPrice = await web3.eth.getGasPrice();
+    console.log("gasPrice", gasPrice);
+    console.log("wallet", wallet);
+    const tx = {
+      from: wallet,
+      to: contractAddress,
+      data,
+      gas: "2000000000", // hard coded for now
+      gasPrice,
+    };
+
+    let gasLimit = await web3.eth.estimateGas(tx);
+    console.log("gasLimit", gasLimit);
+    tx.gas = (gasLimit * 1.5).toFixed(0);
+
+    const res = await web3.eth.sendTransaction(tx, (error, transactionHash) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(`Transaction hash: ${transactionHash}`);
+      }
+    });
+  };
+
+  useEffect(() => {
+    let c = new web3.eth.Contract(abi, contractAddress);
+    setContract(c);
+  }, []);
+
   return (
     <>
+      <Button onClick={add}>Adicionar na white</Button>
+      <Button onClick={vote}>Vote</Button>
       <Wallet />
     </>
   );
