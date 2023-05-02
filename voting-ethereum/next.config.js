94e9c0e8fa40b8a9264f9851082464856882ef59
luucas-melo <lucasmelodos322@gmail.com>
2023-05-02 09:49:23 -0300

    Config eslint and prettier
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- voting-ethereum/next.config.js
+++ voting-ethereum/next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// @ts-check
+const { PHASE_DEVELOPMENT_SERVER, PHASE_TEST } = require('next/constants');
 
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const nextConfig = (phase) => ({
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    ...(phase !== PHASE_DEVELOPMENT_SERVER && phase !== PHASE_TEST && { removeConsole: true }),
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+});
+
+module.exports = nextConfig;
