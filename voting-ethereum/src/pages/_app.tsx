98a5d8e050d20c0fa30a1c9c3d3c566620e3733b
luucas-melo <lucasmelodos322@gmail.com>
2023-05-02 11:03:41 -0300

    Add metamask connection
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- voting-ethereum/src/pages/_app.tsx
+++ voting-ethereum/src/pages/_app.tsx
@@ -1,10 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
+import { MetamaskProvider } from "@/hooks/useMetamask";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <MetamaskProvider>
+        <Component {...pageProps} />
+      </MetamaskProvider>
     </ChakraProvider>
   );
 }
--- voting-ethereum/src/pages/index.tsx
+++ voting-ethereum/src/pages/index.tsx
@@ -1,7 +1,48 @@
-    import { Inter } from 'next/font/google';
+import Wallet from "@/components/Wallet";
+import { useListen } from "@/hooks/useListen";
+import { useMetamask } from "@/hooks/useMetamask";
+import { Button } from "@chakra-ui/react";
+import { Inter } from "next/font/google";
+import { useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  const {
+    dispatch,
+    state: { isMetamaskInstalled, status, wallet },
+  } = useMetamask();
+
+  const listen = useListen();
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      // start by checking if window.ethereum is present, indicating a wallet extension
+      const ethereumProviderInjected = typeof window.ethereum !== "undefined";
+      // this could be other wallets so we can verify if we are dealing with metamask
+      // using the boolean constructor to be explecit and not let this be used as a falsy value (optional)
+      const isMetamaskInstalled =
+        ethereumProviderInjected && Boolean(window.ethereum.isMetaMask);
+
+      const local = window.localStorage.getItem("metamaskState");
+
+      // user was previously connected, start listening to MM
+      if (local) {
+        listen();
+      }
+
+      // local could be null if not present in LocalStorage
+      const { wallet, balance } = local
+        ? JSON.parse(local)
+        : // backup if local storage is empty
+          { wallet: null, balance: null };
+
+      dispatch({ type: "pageLoaded", isMetamaskInstalled, wallet, balance });
+    }
+  }, []);
+  return (
+    <>
+      <Wallet />
+    </>
+  );
 }
