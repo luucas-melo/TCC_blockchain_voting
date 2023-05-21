1db46f1aa14d30eac48bd8b2e3a2da6de63e0906
luucas-melo <lucasmelodos322@gmail.com>
2023-05-21 13:31:00 -0300

    Add textarea component

---
+++ frontend/src/components/FormFields/Textarea.tsx
@@ -0,0 +1,76 @@
+import { ReactElement } from "react";
+import { FieldError, Merge } from "react-hook-form";
+import {
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  forwardRef,
+  Textarea as ChakraTextarea,
+  InputAddonProps,
+  InputElementProps,
+  InputGroup,
+  TextareaProps as ChakraTextareaProps,
+} from "@chakra-ui/react";
+
+export interface InputProps extends ChakraTextareaProps {
+  label?: string | JSX.Element;
+  /**
+   * React Hook Form error object or object containing the error message.
+   *
+   * @example
+   *   const {
+   *     formState: { errors },
+   *   } = useForm<{ password: string }>();
+   *   <Input errors={errors?.password} />;
+   */
+  errors?: FieldError | undefined;
+  rightElement?: ReactElement<InputElementProps>;
+  leftElement?: ReactElement<InputElementProps>;
+  rightAddon?: ReactElement<InputAddonProps>;
+  leftAddon?: ReactElement<InputAddonProps>;
+  helperText?: string;
+}
+
+/**
+ * This component is a wrapper around the Chakra UI Input component.
+ *
+ * Renders a input field with a FormLabel and a FormErrorMessage.
+ *
+ * Also, it wraps the field in a InputGroup to support chakra's InputAddon &
+ * InputElement.
+ *
+ * @see Docs https://chakra-ui.com/docs/components/input/usage#add-elements-inside-input
+ */
+export const Textarea = forwardRef<InputProps, "input">((props, ref) => {
+  const {
+    label,
+    errors,
+    rightElement,
+    leftElement,
+    rightAddon,
+    leftAddon,
+    size,
+    width,
+    flex,
+    helperText,
+    ...rest
+  } = props;
+
+  return (
+    <FormControl
+      isInvalid={Boolean(errors)}
+      width={width || "100%"}
+      isRequired={props?.required || props?.isRequired}
+      isDisabled={props?.isDisabled}
+      flex={flex}
+    >
+      {label && <FormLabel>{label}</FormLabel>}
+      <InputGroup size={size}>
+        <ChakraTextarea {...rest} ref={ref} />
+      </InputGroup>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+      <FormErrorMessage color="red.300">{errors?.message}</FormErrorMessage>
+    </FormControl>
+  );
+});
